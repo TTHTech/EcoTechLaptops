8b04088f139abae14bb7deded9e3390460d38e29
antrandothanh <trandothanhan04@gmail.com>
2024-05-11 15:06:39 +0700

    finished statistics page

--- src/main/java/org/example/test/controller/AdminStatisticsController.java
+++ src/main/java/org/example/test/controller/AdminStatisticsController.java
@@ -30,10 +30,30 @@ public class AdminStatisticsController {
 
     @GetMapping("/admin/statistics/day")
     public String adminStatistics_filterByDay(Model model) {
-        model.addAttribute("filterBy", "THEO NGÀY");
+        model.addAttribute("filterBy", "THEO NGÀY HÔM NAY");
         model.addAttribute("totalPriceOfAllOrders", orderService.getTotalPriceOfAllOrdersByDay());
         model.addAttribute("theNumberOfAllProductsSold", orderService.getTheNumberOfProductsSoldInAllOrdersByDay());
         model.addAttribute("orders", orderService.getAllOrdersByDay());
+        model.addAttribute("products", productService.getAllProduct());
+        return "admin/statistics";
+    }
+
+    @GetMapping("/admin/statistics/week")
+    public String adminStatistics_filterByWeek(Model model) {
+        model.addAttribute("filterBy", "THEO TUẦN NÀY");
+        model.addAttribute("orders", orderService.getAllOrdersByWeek());
+        model.addAttribute("totalPriceOfAllOrders", orderService.getTotalPriceOfAllOrdersByWeek());
+        model.addAttribute("theNumberOfAllProductsSold", orderService.getTheNumberOfProductsSoldInAllOrdersByWeek());
+        model.addAttribute("products", productService.getAllProduct());
+        return "admin/statistics";
+    }
+
+    @GetMapping("/admin/statistics/month")
+    public String adminStatistics_filterByMonth(Model model) {
+        model.addAttribute("filterBy", "THEO THÁNG NÀY");
+        model.addAttribute("orders", orderService.getAllOrdersByMonth());
+        model.addAttribute("totalPriceOfAllOrders", orderService.getTotalPriceOfAllOrdersByMonth());
+        model.addAttribute("theNumberOfAllProductsSold", orderService.getTheNumberOfProductsSoldInAllOrdersByMonth());
         model.addAttribute("products", productService.getAllProduct());
         return "admin/statistics";
     }
--- src/main/java/org/example/test/service/OrderService.java
+++ src/main/java/org/example/test/service/OrderService.java
@@ -13,9 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 
 @Service
@@ -97,6 +96,97 @@ public class OrderService {
     }
 
     public List<Order> getAllOrdersByDay() {
-        return orderRepository.findOrdersByDate("2024-05-10");
+        String currentDay = getCurrentDay();
+        return orderRepository.findOrdersByDate(currentDay);
+    }
+
+    // lấy tất cả đơn hàng trong tuần của ngày hiện tại
+    public List<Order> getAllOrdersByWeek() {
+        Date today_date = new Date();
+        // lấy số tuần trong tháng của ngày hiện tại
+        int todayWeekNumber = getWeekOfMonth(today_date);
+        int month = today_date.getMonth();
+        int year = today_date.getYear();
+
+        List<Order> orders = orderRepository.findAll();
+        List<Order> ordersByWeek = new ArrayList<Order>();
+        for (Order order : orders) {
+            Date orderCreateDate = order.getCreateDate();
+            // kiểm tra đơn hàng phải cùng tháng và cùng ngày
+            if (orderCreateDate.getMonth() == month && orderCreateDate.getYear() == year) {
+                // sau đó bắt đầu kiểm tra dơn hàng phải cùng số tuần trong tháng
+                int orderWeekNumber = getWeekOfMonth(orderCreateDate);
+                if (orderWeekNumber == todayWeekNumber) {
+                    ordersByWeek.add(order);
+                }
+            }
+        }
+        return ordersByWeek;
+    }
+
+    public List<Order> getAllOrdersByMonth() {
+        // lấy tháng và năm của ngày hôm nay
+        Date today_date = new Date();
+        int month = today_date.getMonth();
+        int year = today_date.getYear();
+        List<Order> orders = orderRepository.findAll();
+        List<Order> ordersByMonth = new ArrayList<Order>();
+        for (Order order : orders) {
+            Date orderCreateDate = order.getCreateDate();
+            // kiểm tra đơn hàng phải cùng tháng và cùng năm
+            if (orderCreateDate.getMonth() == month && orderCreateDate.getYear() == year) {
+                ordersByMonth.add(order);
+            }
+        }
+        return ordersByMonth;
+    }
+
+    public String getCurrentDay() {
+        Date today_date = new Date();
+        String today_string = new SimpleDateFormat("yyyy-MM-dd").format(today_date);
+        return today_string;
+    }
+
+
+
+    // lấy ra số tuần của trong 1 tháng
+    public int getWeekOfMonth(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int weekNumberOfMonth = calendar.get(Calendar.WEEK_OF_MONTH);
+        return weekNumberOfMonth;
+    }
+
+
+    public double getTotalPriceOfAllOrdersByWeek() {
+        double total = 0;
+        for (Order order : getAllOrdersByWeek()) {
+            total = total + order.getTotalPrice();
+        }
+        return total;
+    }
+
+    public int getTheNumberOfProductsSoldInAllOrdersByWeek() {
+        int count = 0;
+        for (Order order : getAllOrdersByWeek()) {
+            count = count + order.getTheNumberOfProduct();
+        }
+        return count;
+    }
+
+    public double getTotalPriceOfAllOrdersByMonth() {
+        double total = 0;
+        for (Order order : getAllOrdersByMonth()) {
+            total = total + order.getTotalPrice();
+        }
+        return total;
+    }
+
+    public int getTheNumberOfProductsSoldInAllOrdersByMonth() {
+        int count = 0;
+        for (Order order : getAllOrdersByMonth()) {
+            count = count + order.getTheNumberOfProduct();
+        }
+        return count;
     }
 }
