31131cbca03252c8c5d3f4672e7afdf03078ad41
HunggSamm <143309756+HunggSamm@users.noreply.github.com>
2024-05-12 08:44:12 +0700

    ok r

--- src/main/java/org/example/test/controller/AdminProductController.java
+++ src/main/java/org/example/test/controller/AdminProductController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -33,6 +34,36 @@ public class AdminProductController {
         this.cartService = cartService;
     }
 
+    @GetMapping("/admin/product")
+    public String adminProductPage(Model model) {
+        List<Product> products = productService.getAllProduct();
+        List<Category> categories = categoryService.getCategoriesByStatus("on");
+        model.addAttribute("products", products);
+        model.addAttribute("categories", categories);
+        return "admin/product";
+    }
+
+    @PostMapping("/admin/product")
+    public String adminProductPage(@RequestParam(value = "categoryId", required = false) Long categoryId,
+            @RequestParam(value = "productName", required = false) String productName,
+            Model model) {
+        List<Product> products;
+        if (productName != null && !productName.isEmpty()) {
+            // Nếu tìm theo tên sản phẩm
+            products = productService.getProductsByName(productName);
+        } else if (categoryId != null) {
+            // Nếu tìm theo danh mục
+            products = productService.getProductsByCategoryId(categoryId);
+        } else {
+            // Không có thông tin tìm kiếm, hiển thị tất cả sản phẩm
+            products = productService.getAllProduct();
+        }
+        List<Category> categories = categoryService.getAllCategory();
+        model.addAttribute("products", products);
+        model.addAttribute("categories", categories);
+        return "admin/product";
+    }
+
     @PostMapping("/admin/product/create")
     public String handleCreateProduct(@ModelAttribute("newProduct") Product product, Model model,
             @RequestParam("imageProduct") MultipartFile file) {
@@ -51,6 +82,75 @@ public class AdminProductController {
         product.setCategory(category);
         product.setStatus("on");
         this.productService.saveProduct(product);
+        return "redirect:/admin/product";
+    }
+
+    @GetMapping("/admin/product/create")
+    public String handleCreateProduct(Model model) {
+
+        List<Category> categories = categoryService.getCategoriesByStatus("on");
+        model.addAttribute("categories", categories);
+        model.addAttribute("newProduct", new Product());
+        return "admin/createProduct";
+    }
+
+    @PostMapping("/admin/product/delete")
+    public String handleDeleteProduct(@RequestParam("id") Long id, Model model) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            // Cập nhật trạng thái của sản phẩm thành "off"
+            product.setStatus("off");
+            productService.saveProduct(product);
+        }
+        return "redirect:/admin/product";
+    }
+
+    @GetMapping("/admin/product/update/{id}")
+    public String pageUpdateProduct(@PathVariable("id") Long id, Model model) {
+        Product currentProduct = productService.getProductById(id);
+        List<Category> categories = categoryService.getAllCategory();
+
+        model.addAttribute("categories", categories);
+        model.addAttribute("newProduct", currentProduct);
+        return "admin/updateProduct";
+    }
+
+    @PostMapping("/admin/product/update")
+    public String handleUpdateProduct(@ModelAttribute("newProduct") Product product, Model model,
+            @RequestParam("imageProduct") MultipartFile file) {
+        Product currentProduct = productService.getProductById(product.getId());
+        if (!currentProduct.getName().equals(product.getName())) {
+            if (!currentProduct.getName().equals(product.getName())
+                    && productService.isProductExistsInCategory(product.getName(), product.getCategory().getName())) {
+                // Nếu trùng, thêm thông báo lỗi và trả về trang cập nhật sản phẩm với thông báo
+                // lỗi
+                model.addAttribute("error", "Tên sản phẩm đã tồn tại trong danh mục này!");
+                List<Category> categories = categoryService.getAllCategory();
+                model.addAttribute("categories", categories);
+                model.addAttribute("newProduct", product);
+                return "admin/updateProduct";
+            }
+        }
+
+        currentProduct.setName(product.getName());
+        currentProduct.setPrice(product.getPrice());
+        currentProduct.setDescription(product.getDescription());
+        Category category = categoryService.findByName(product.getCategory().getName());
+        String imageProduct = this.uploadService.handleSaveUploadFile(file, "product");
+        if (imageProduct != "")
+            currentProduct.setImage(imageProduct);
+        currentProduct.setCategory(category);
+        productService.saveProduct(currentProduct);
+        return "redirect:/admin/product";
+    }
+
+    @PostMapping("admin/product/deleteAll")
+    public String handleDeleteAllProduct(Model model) {
+        List<Product> products = productService.getAllProduct();
+        for (Product product : products) {
+            product.setStatus("off");
+            productService.saveProduct(product);
+        }
         return "redirect:/admin/product";
     }
 }
